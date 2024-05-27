c97e5d974a53bd3b47d0ec430edad46ed1463fa9
Kimkong <rjaehno2@naver.com>
2024-05-27 17:10:04 +0900

    Merge DB setting with current work

--- front-end/fos_app/urls.py
+++ front-end/fos_app/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login.html/', views.login_view, name='login'),
-    path('login.html/signup.html/', views.signup_view, name='signup'),
+    #path('login.html/signup.html/', views.signup_view, name='signup'),
+    path('signup/',       views.signup, name='signup'),
     path('login.html/forgotpassword.html/', views.forgotpassword_view, name='forgotpassword'),
     path('setting.html/', views.setting_view, name='setting'),
+    path('temp-fine/',    views.temp_fine_view, name='temp_fine'),
+    path('login.html/forgotpassword.html/forgotpasswordremake/', views.forgot_password_remake_view, name='forgot_password_remake'),
 ]
