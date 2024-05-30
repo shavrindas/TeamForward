f504249ca648d809fadc7184b526f79f49a0ddc2
Kimkong <rjaehno2@naver.com>
2024-05-31 00:07:52 +0900

    chore: Add template path directory to sttings.py

---
+++ TFProject/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(''             , views.main_page,  name='main_page'),
+    path('signup/'      , views.signup,     name='signup'),
+    path('login/'       , views.login,      name='login'),
+    path('logout/'      , views.logout,     name='logout'),
+    path('user_session/', views.user_session, name='user_session'),  # URL 패턴 수정
+    path('find_account/', views.find_account, name='find_account'),
+    path('edit_account/', views.edit_account, name='edit_account'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+   
+
+]
