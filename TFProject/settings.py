8b51514a4c11e348b17bf53cabe5ec0c472f624b
shavrindas <ynhamin@gmail.com>
2024-06-05 10:34:31 +0900

    add&form
    합스부르크식 코드 결혼식

--- TFProject/settings.py
+++ TFProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,6 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+TEMPLATE_ACCOUNT = os.path.join(BASE_DIR, "accounts", "templates") # 하나하나 추가.
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ua!$s(s@&z_qbk)74lvg-j9mhn_37rri)=ez+lwgt+6m&oa6%_'
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'add_clothes',
+    'recommendation_clothes',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +59,9 @@ ROOT_URLCONF = 'TFProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_ACCOUNT, #요거도 하나하나 추가.
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,9 +130,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIR = [     ## 이 폴더 자체를 새로 만듦.
+    #여기에 인터넷에 본 대로 각 app에 대한 경로 설정
+    os.path.join(BASE_DIR, 'accounts', 'static', 'accounts'),
+    #~/accounts/static
+]
 
-
-import os
 # 추가한 내용
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
