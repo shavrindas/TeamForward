38d87e37dfdf42579b1873a387a2db8a9a60a5d6
51s <162551685+51spring@users.noreply.github.com>
2024-06-08 17:23:49 +0900

    modity route user_pictures

--- recommend/utils.py
+++ recommend/utils.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from django.conf import settings
 import os
 
+import os
+
 
 def read_class_indices_clothesdata():
     file_path = settings.BASE_DIR / 'add_clothes' / 'model' / 'class_indices.json'
@@ -13,7 +15,9 @@ def read_class_indices_clothesdata():
 import os
 
 def get_user_clothes():
-    pictures_directory = 'C:/TeamForward-recommendation/media/user_pictures'  # 사용자 사진이 저장된 디렉토리 경로
+    current_directory = os.getcwd()
+    pictures_directory = os.path.join(current_directory, 'media', 'user_pictures')
+    #pictures_directory = 'C:/TeamForward-recommendation/media/user_pictures'  # 사용자 사진이 저장된 디렉토리 경로
     user_pictures = []
 
     # 사용자 사진 디렉토리의 모든 파일에 대해 반복
