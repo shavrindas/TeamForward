39971cbc1b512d9261a720f545c684a1a52d5ab9
shavrindas <ynhamin@gmail.com>
2024-06-19 01:59:54 +0900

    최종통합 2
    최종통합 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(''             , views.main_page,  name='main_page'),
@@ -12,4 +14,7 @@ urlpatterns = [
     path('edit_account/', views.edit_account, name='edit_account'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
    
-]
+]   
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
